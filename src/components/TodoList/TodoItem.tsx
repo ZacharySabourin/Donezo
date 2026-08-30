c0942aa520e15fce7455c27667a2c8c1892378de
Zachary Sabourin <zachary.sabourin@gmail.com>
2026-08-30 15:49:40 -0400

    feat: Add filters, item count, and delete all button

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -3,8 +3,6 @@ import TodoCheck from "./TodoCheck";
 import TodoDeleteButton from "./TodoDeleteButton";
 import TodoText from "./TodoText";
 
-
-
 export default function TodoItem({
   todo,
   onRefetch,
@@ -12,13 +10,19 @@ export default function TodoItem({
   todo: Todo;
   onRefetch: () => void;
 }>) {
-  
-
   return (
     <div>
-      <TodoCheck todoId={todo.id} initialValue={todo.completed} onRefetch={onRefetch}/>
-      <TodoText todoId={todo.id} initialValue={todo.text} onRefetch={onRefetch}/>
-      <TodoDeleteButton todoId={todo.id} onRefetch={onRefetch}/>
+      <TodoCheck
+        todoId={todo.id}
+        initialValue={todo.completed}
+        onRefetch={onRefetch}
+      />
+      <TodoText
+        todoId={todo.id}
+        initialValue={todo.text}
+        onRefetch={onRefetch}
+      />
+      <TodoDeleteButton todoId={todo.id} onRefetch={onRefetch} />
     </div>
   );
 }
--- src/components/TodoList/TodoListSection.tsx
+++ src/components/TodoList/TodoListSection.tsx
@@ -1,28 +1,110 @@
 import { useState } from "react";
 import useFetchSortedTodos from "../../hooks/useFetchSortedTodos";
+import ApiError from "../../types/ApiError";
 import CreationForm from "./CreationForm";
 import ListDisplay from "./ListDisplay";
+import type Todo from "../../types/Todo";
+import useUpdateEffect from "../../hooks/useUpdateEffect";
 
 const userId: string = import.meta.env.VITE_USER_ID;
+const baseUrl: string = import.meta.env.VITE_SERVER_API_BASE_URL;
 
 export default function TodoListSection() {
   const [refreshTrigger, setRefreshTrigger] = useState<boolean>(false);
   const triggerRefresh = () => setRefreshTrigger((prev) => !prev);
+  const [selectedFilter, setSelectedFilter] = useState<string>("all");
 
   const { todos, loading, error, refetch } = useFetchSortedTodos(
     userId,
     refreshTrigger,
   );
 
+  const [filteredTodos, setFilteredTodos] = useState<Todo[]>(todos);
+
+  function handleDeleteAllCompleted() {
+    const toDelete: Todo[] = todos.slice().filter((todo) => todo.completed);
+    if (toDelete.length != 0) {
+      fetch(baseUrl, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(toDelete),
+      })
+        .then((response: Response) => {
+          if (!response.ok) {
+            throw new ApiError("Error deleting Todos", response.status);
+          }
+          triggerRefresh();
+        })
+        .catch((error: ApiError) => {
+          alert(error.message);
+        });
+    }
+  }
+
+  useUpdateEffect(() => {
+    setFilteredTodos(() => {
+      if (selectedFilter === "active") {
+        return todos.slice().filter((todo) => !todo.completed);
+      }
+      if (selectedFilter === "completed") {
+        return todos.slice().filter((todo) => todo.completed);
+      }
+      return todos;
+    });
+  }, [todos, selectedFilter]);
+
   return (
     <div id="todo-list">
       <CreationForm onSaveSuccess={triggerRefresh} />
       <ListDisplay
-        todos={todos}
+        todos={filteredTodos}
         loading={loading}
         error={error}
         refetch={refetch}
       />
+      <div id="options">
+        <p>{todos.length} items left</p>
+        <form>
+          <label className="list-filter">
+            <input
+              type="radio"
+              name="choice"
+              value="all"
+              checked={selectedFilter === "all"}
+              onChange={(e) => setSelectedFilter(e.target.value)}
+            />
+            <span>All</span>
+          </label>
+
+          <label className="list-filter">
+            <input
+              type="radio"
+              name="choice"
+              value="active"
+              checked={selectedFilter === "active"}
+              onChange={(e) => setSelectedFilter(e.target.value)}
+            />
+            <span>Active</span>
+          </label>
+
+          <label className="list-filter">
+            <input
+              type="radio"
+              name="choice"
+              value="completed"
+              checked={selectedFilter === "completed"}
+              onChange={(e) => setSelectedFilter(e.target.value)}
+            />
+            <span>Completed</span>
+          </label>
+        </form>
+
+        <button type="button" onClick={handleDeleteAllCompleted}>
+          Clear all Completed
+        </button>
+      </div>
     </div>
   );
 }
